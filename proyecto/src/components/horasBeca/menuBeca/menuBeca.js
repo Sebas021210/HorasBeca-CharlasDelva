40fd6e68b86f511f92b577a90f600cdc8e371f95
ManuelR11 <rod21509@uvg.edu.gt>
2023-05-11 16:00:49 -0600

    Ejemplo

---
+++ proyecto/src/components/horasBeca/menuBeca/menuBeca.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Componente1 = () => {
+  return (
+    <div>
+      <h1>Componente1</h1>
+    </div>
+  );
+};
+
+export default Componente1;
