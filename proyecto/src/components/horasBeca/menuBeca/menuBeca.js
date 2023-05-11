f47c507fb7d83e48680393c3f7ed999684355fa4
ManuelR11 <rod21509@uvg.edu.gt>
2023-05-11 16:33:28 -0600

    Creacion de menu sidebar

--- proyecto/src/components/horasBeca/menuBeca/menuBeca.js
+++ proyecto/src/components/horasBeca/menuBeca/menuBeca.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import {AiOutlineCalendar} from "react-icons/ai";
+import {AiOutlinePlus} from "react-icons/ai";
+import {AiOutlineTeam} from "react-icons/ai";
+import './sidebar.scss'
 
-const Componente1 = () => {
-  return (
-    <div>
-      <h1>Componente1</h1>
-    </div>
-  );
-};
 
-export default Componente1;
+export default function Sidebar() {
+    return (
+        <div id ="sidebar">
+            <div className="top__links">
+                <div className="logo">
+                <img
+                src="https://camtur.org/wp-content/uploads/2021/01/Logo-UVG-002.jpg"
+                alt="Delvalle"
+                />
+                </div>
+                <ul>
+                    <li>
+                        <AiOutlinePlus style={{ fontSize: "27.5px" }}/>
+                        <span>Nueva Actividad</span>
+                    </li>
+                    <li>
+                        <AiOutlineCalendar style={{ fontSize: "27.5px" }}/>
+                        <span>Calendario</span>
+                    </li>
+                    <li>
+                        <AiOutlineTeam style={{ fontSize: "27.5px", color: "white" }}/>
+                        <span>Historial alumnos</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
