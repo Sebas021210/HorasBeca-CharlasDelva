cf8e15f05ace9fe5eba6106baff130d57e36570b
Sebastián Solorzano <sol21826@uvg.edu.gt>
2023-05-26 12:56:33 -0600

    Login funcional

--- proyecto/src/components/login/login.js
+++ proyecto/src/components/login/login.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import './login.css';
 
 export default function Login() {
-    const navigateBeca = useNavigate();
-    const navigateDelva = useNavigate();
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const handleClickBeca = () => {
-        navigateBeca('/menuBeca');
-    };
+    const handleClick = async (e) => {
+        e.preventDefault() 
+        try {
+            const response = await fetch(`http://localhost:5050/usuarios`);
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                const usuarioEncontrado = data.find(usuarios => email === usuarios.correo && password === usuarios.contraseña);
+                console.log(usuarioEncontrado);
 
-    const handleClickDelva = () => {
-        navigateDelva('/menuDelva');
+                if (usuarioEncontrado) {
+                    const idRol = usuarioEncontrado.id_rol;
+                    if (idRol === 2) {
+                        navigate('/menuBeca');
+                    } else if (idRol === 3) {
+                        navigate('/menuDelva');
+                    } else {
+                        console.log('Error: Rol no encontrado');
+                    }
+                } else {
+                    console.log('Error: Usuario no encontrado');
+                }
+            } else {
+                console.log('Error: No se pudo obtener la lista de usuarios');
+            }
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     return (
@@ -21,11 +44,26 @@ export default function Login() {
                     <img src="https://tienda.uvg.edu.gt/attach/tiendas/Logo-vertical-verde_5a8b26cb76441.jpg" alt="Logo" />
                     <div className="form_div">
                         <label>Login:</label>
-                        <input className="field_class" name="login_txt" type="text" placeholder="Ingrese su usuario" autoFocus />
+                        <input
+                            className="field_class"
+                            name="user_txt"
+                            type="text"
+                            placeholder="Ingrese su usuario"
+                            autoFocus
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                         <label>Password:</label>
-                        <input id="pass" className="field_class" name="password_txt" type="password" placeholder="Ingrese su contraseña" />
-                        <button className="submit_class" onClick={handleClickBeca}>Iniciar Sesión Beca</button>
-                        <button className="submit_class" onClick={handleClickDelva}>Iniciar Sesión Delva</button>
+                        <input
+                            id="pass"
+                            className="field_class"
+                            name="password_txt"
+                            type="password"
+                            placeholder="Ingrese su contraseña"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <button className="submit_class" onClick={handleClick}>Iniciar Sesión</button>
                     </div>
                 </form>
             </div>
